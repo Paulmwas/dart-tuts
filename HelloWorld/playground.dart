4a7ca09c49027cac025b88e28713389e6fb42f96
Paul <mwapaul265@gmail.com>
2025-08-11 17:47:32 +0300

    This is just a hello world

---
+++ HelloWorld/playground.dart
@@ -0,0 +1,38 @@
+// main(){
+//   print('Hello World');
+// }
+// dart:core -- package
+// import 'dart:core';
+// import 'dart:io';
+// main(){
+//   var firstName = 'Paul';
+//   String lastName = 'Mwangi';
+//   print(firstName + ' ' +lastName);
+//   stdout.writeln("What is your name?");
+//   var userName = stdin.readLineSync();
+//   print('Your name is $userName');
+// }
+// -inline comments
+/*
+Block comment
+*/
+
+/// Documentation
+/// 
+/// 
+// Data Types
+main(){
+  int amount1 = 400;
+  var amount2 = 300;
+  print("The first amount is $amount1 | The second amount is $amount2");
+  double damount = 34.45;
+  var damount3 = 45.90;
+  print('The amount of sugar is $damount kgs and you have $damount3 kgs. Please unfat');
+  var isTrue = true;
+  bool isTrue2 = false;
+  print('isItTrue $isTrue | IsIttrue $isTrue2');
+  dynamic weakVariable = 500;
+  weakVariable = 'This is onsense';
+  print(weakVariable);
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Static typed, compiled
+
+exc - dynamic
+AOT
+JIT
+
+Data Types
+Strongly typed
+Dynamically typed
+
+Int
+double
+String
+bool
+dynamic
